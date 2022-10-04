e4aec34f78ec3bf38a4e6cb7dc736106afc386af
Aqeel Shahzad <67902032+aqeel-spec@users.noreply.github.com>
2022-10-04 06:13:24 +0500

    completed assignment upto 17
    arrays topic is almost completed

--- p-15/p-15.js
+++ p-15/p-15.js
@@ -1,15 +1,9 @@
 var guests = ['Muzammal', 'Haider', 'Farooq'];
+var deletedguest = guests.pop();
+var deletedguestMsg = deletedguest + ' is not comming at today party ';
+guests.push('Noman');
 var msg = " Come and join us at 9:00 PM party ";
-var oldgst = 'Above are the list of old gusets';
-//       here is the list of new added guests
-guests.push('Noman', 'Faida');
-var rmpersonArray = guests.splice(2, 0, 'Muhammad Farooq');
-//  at this stage we have 6 guests
-// I am going to remove last one
-guests.pop();
 console.log(guests[0] + msg);
 console.log(guests[1] + msg);
-console.log(guests[2] + msg);
-console.log(oldgst);
-console.log(guests);
-console.log('notthig = ' + rmpersonArray);
+console.log(guests[2] + msg + '\t\n');
+console.log(deletedguestMsg);
--- p-15/p-15.ts
+++ p-15/p-15.ts
@@ -1,15 +1,11 @@
-let guests: string[] = ['Muzammal', 'Haider', 'Farooq'];
+const guests: string[] = ['Muzammal', 'Haider', 'Farooq'];
+const deletedguest = guests.pop();
+const deletedguestMsg =  deletedguest +  ' is not comming at today party ';
+guests.push('Noman');
+
 let msg: string = " Come and join us at 9:00 PM party ";
-let oldgst: string = 'Above are the list of old gusets';
-//       here is the list of new added guests
-guests.push('Noman', 'Faida');
-guests.splice(2,0,'Muhammad Farooq');//at 2nd index name added
-//  at this stage we have 6 guests
-// I am going to remove last one
-guests.pop()
 
 console.log(guests[0] + msg)
 console.log(guests[1] + msg)
-console.log(guests[2] + msg)
-console.log(oldgst);
-console.log(guests)
+console.log(guests[2] + msg + '\t\n')
+console.log(deletedguestMsg);
