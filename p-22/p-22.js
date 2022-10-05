4bee4157e8edd93d913783f13b1d124d58b66c48
Aqeel Shahzad <67902032+aqeel-spec@users.noreply.github.com>
2022-10-05 16:41:28 +0500

    23 task completed
    cOMMING VERY SOON

---
+++ p-22/p-22.js
@@ -0,0 +1,8 @@
+var persons = {
+    name: {
+        first: ['kia', 'bt', 'h'],
+        last: 'person2'
+    }
+};
+console.log(persons.name.first[0]);
+console.log(persons.name.last);
---
+++ p-22/p-22.ts
@@ -0,0 +1,9 @@
+const persons = {
+    name: {
+      first: ['kia','bt','h'],
+      last: 'person2',
+    },
+    // …
+};
+console.log(persons.name.first[0]);
+console.log(persons.name.last);
